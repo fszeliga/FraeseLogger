a6e74460c958b1e9d8ff9447991b134ce107a800
Filip <Filip@FILIP-DESKTOP>
2016-09-20 12:42:39 +0200

    asdads

---
+++ log_comp/data/impl/cncStepsTurn.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace imi_cnc_logger.log_comp.data.impl
+{
+    class cncStepsTurn : CNCDataGenericBase<int>
+    {
+ 
+        internal override string Description
+        {
+            get
+            {
+                return "StepsTurn X";
+            }
+        }
+
+        internal override string Key
+        {
+            get
+            {
+                return "stepsTurnX";
+            }
+        }
+
+        internal override string Name
+        {
+            get
+            {
+                return "asd";
+            }
+        }
+
+        public override string asString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string getValue(string[] args)
+        {
+            return Value.ToString();
+        }
+
+        public override void initialize()
+        {
+            Value = 0;
+        }
+
+        public override bool read()
+        {
+           Value = myConn.SMCSettings.AxisX.StepsTurn;
+
+            return true;
+        }
+    }
+}
---
+++ log_comp/data/impl/cncWayTurn.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace imi_cnc_logger.log_comp.data.impl
+{
+    class cncWayTurn : CNCDataGenericBase<double>
+    {
+ 
+        internal override string Description
+        {
+            get
+            {
+                return "WayTurn X";
+            }
+        }
+
+        internal override string Key
+        {
+            get
+            {
+                return "wayTurnX";
+            }
+        }
+
+        internal override string Name
+        {
+            get
+            {
+                return "asd";
+            }
+        }
+
+        public override string asString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string getValue(string[] args)
+        {
+            return Value.ToString();
+        }
+
+        public override void initialize()
+        {
+            Value = 0;
+        }
+
+        public override bool read()
+        {
+           Value = myConn.SMCSettings.AxisX.WayTurn;
+
+            return true;
+        }
+    }
+}
